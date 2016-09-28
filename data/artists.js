4e04ae1a4777cdad1f1079cac1e75e1142b382af
davojta <damarah@gmail.com>
2016-09-28 21:41:01 +0300

    add data files

---
+++ data/artists.js
@@ -0,0 +1,33 @@
+var artists = [
+  {
+    id: 'm_shmatova',
+    name: 'Марта Шматова',
+    fb: '',
+    link: ''
+  },
+  {
+    id: 'i_kosobuko',
+    name: 'Илона Кособуко',
+    fb: '',
+    link: ''
+  },
+  {
+    id: 'e_sumareva',
+    name: 'Екатерина Сумарева',
+    fb: '',
+    link: ''
+  },
+  {
+    id: 't_radivilko',
+    name: 'Татьяна Радивилко',
+    fb: '',
+    link: ''
+  },
+  {
+    id: 'a_silivonchik',
+    name: 'Анна Силивончик',
+    fb: '',
+    link: ''
+
+  }
+]
---
+++ data/studios.js
@@ -0,0 +1,42 @@
+var studios = [
+	{
+		coords: [
+			27.590049505233765,
+			53.927724646237046
+		],
+		n: 1,
+		address: 'ул Сурганова 42, 44',
+		artists: [
+			'm_shmatova',
+			'i_kosobuko',
+			'e_sumareva'
+
+		],
+		landmark: 'болконы лодочки'
+	},
+	{
+		coords: [
+			27.59344518184662,
+			53.93149268335978
+		],
+		n: 2,
+		address: 'ул Некрасова 11',
+		artists: [
+			't_radivilko'
+
+		],
+		landmark: 'кафе Кавказкая пленница'
+	},
+	{
+		coords: [
+			27.588633298873898,
+			53.92515978599045
+		],
+		n: 3,
+		address: 'ул Беломорская 21',
+		artists: [
+			'a_silivonchik'
+		],
+		landmark: 'ЖК Флагман'
+	}
+]
