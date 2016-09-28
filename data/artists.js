eba0961827cc935410e89b2b303884dbbbb7bb22
aliona-gorbachova <aliona.by@mail.ru>
2016-09-29 02:14:16 +0300

    Update artists.js

--- data/artists.js
+++ data/artists.js
@@ -2,41 +2,106 @@ var artists = [
   {
     id: 'm_shmatova',
     name: 'Марта Шматова',
-    fb: '',
+    fb: 'https://www.facebook.com/marta.shmatava',
     link: '',
-    photo: '',
+    photo: 'm_shmatava.jpg',
     text: ''
   },
   {
     id: 'i_kosobuko',
     name: 'Илона Кособуко',
-    fb: '',
+    fb: 'https://www.facebook.com/ilona.letters',
     link: '',
-    photo: '',
+    photo: 'i_kosobuko.jpg',
     text: ''
   },
   {
     id: 'e_sumareva',
     name: 'Екатерина Сумарева',
-    fb: '',
-    link: '',
-    photo: '',
+    fb: 'https://www.facebook.com/sumarava.katsiaryna',
+    link: 'http://sumarava.carbonmade.com/',
+    photo: 'k_sumarava.jpg',
     text: ''
   },
   {
     id: 't_radivilko',
     name: 'Татьяна Радивилко',
+    fb: 'https://www.facebook.com/tatiana.radsivilko',
+    link: 'http://radsivilko.jimdo.com/',
+    photo: 't_radsivilko.jpg',
+    text: ''
+  },
+  {
+    id: 'o_ugrinovich',
+    name: 'Ольга Угринович',
+    fb: 'https://www.facebook.com/olugrinovich',
+    link: 'http://www.ugrinovich.com/',
+    photo: 'o_ugrinovich.jpg',
+    text: ''
+  },
+  {
+    id: 'o_sazykina',
+    name: 'Ольга Сазыкина',
+    fb: 'https://www.facebook.com/sarakizyna',
+    link: '',
+    photo: 'o_sazykina.jpg',
+    text: ''
+  },
+  {
+    id: 'e_shnegel',
+    name: 'Елена Шнегель',
+    fb: 'https://www.facebook.com/profile.php?id=100005230596150',
+    link: '',
+    photo: 'e_shnegel.jpg',
+    text: ''
+  },
+  {
+    id: 'o_krupenkova',
+    name: 'Ольга Крупенкова',
+    fb: 'https://www.facebook.com/olga.krupenkova',
+    link: '',
+    photo: 'o_krupenkova.jpg',
+    text: ''
+  },
+  {
+    id: 'a_silivonchik',
+    name: 'Анна Силивончик',
+    fb: 'https://www.facebook.com/anna.silivonchik',
+    link: 'http://silivonchik.ru/',
+    photo: 'a_silivonchik.jpg',
+    text: ''
+  },
+  {
+    id: '',
+    name: '',
     fb: '',
     link: '',
     photo: '',
     text: ''
   },
   {
-    id: 'a_silivonchik',
-    name: 'Анна Силивончик',
+    id: '',
+    name: '',
     fb: '',
     link: '',
     photo: '',
     text: ''
-  }
+  },
+  {
+    id: '',
+    name: '',
+    fb: '',
+    link: '',
+    photo: '',
+    text: ''
+  },
+  {
+    id: '',
+    name: '',
+    fb: '',
+    link: '',
+    photo: '',
+    text: ''
+  },
+  
 ]
