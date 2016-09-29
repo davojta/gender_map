65720d3b7a5c14ad8e3d3bddd983f38852705684
aliona-gorbachova <aliona.by@mail.ru>
2016-09-29 03:24:55 +0300

    Update studios.js

--- data/studios.js
+++ data/studios.js
@@ -22,7 +22,11 @@ var studios = [
 		n: 2,
 		address: 'ул Некрасова 11',
 		artists: [
-			't_radivilko'
+			't_radivilko',
+			'o_ugrinovich',
+			'o_sazykina',
+			'e_shnegel',
+			'o_krupenkova'
 
 		],
 		landmark: 'кафе Кавказкая пленница'
@@ -38,5 +42,103 @@ var studios = [
 			'a_silivonchik'
 		],
 		landmark: 'ЖК Флагман'
-	}
+	},
+        {
+		coords: [
+			27.578998804092407,
+                        53.909615636392466
+		],
+		n: 4,
+		address: 'ул. Козлова, 2',
+		artists: [
+			'a_slobodchikova',
+			'n_liventseva'
+		],
+		landmark: 'магазин "Океан"'
+	},
+        {
+		coords: [
+			27.57134109735489,
+                        53.91108815556854
+		],
+		n: 5,
+		address: 'ул. Киселёва, 11',
+		artists: [
+			'g_romanova'
+		],
+		landmark: 'мемориальная доска подпольщицы Марии Осиповой'
+	},
+        {
+		coords: [
+			  27.577563822269436,
+                          53.90155536011861
+		],
+		n: 6,
+		address: 'ул.  Первомайская, 24А, К1',
+		artists: [
+			'z_lutsevich'
+		],
+		landmark: 'магазин "Сделай Сам"'
+	},
+        {
+		coords: [
+			27.55192458629608,
+                        53.89188445293951
+		],
+		n: 7,
+		address: 'ул. Кирова, 2',
+		artists: [
+			'j_kapustnikova'
+		],
+		landmark: ''
+	},
+        {
+		coords: [
+			27.55689203739166,
+                        53.89992921114309
+		],
+		n: 8,
+		address: 'пр-т Независимости, 19',
+		artists: [
+			'n_belookaya'
+		],
+		landmark: 'здание ГУМа, кафе "У Франциска", мемориальная доска Стефании Станюты'
+	},
+        {
+		coords: [
+			27.54921555519104,
+                        53.9051946070535
+		],
+		n: 9,
+		address: 'ул. Раковская, 16а',
+		artists: [
+			'i_demina'
+		],
+		landmark: ''
+	},
+        {
+		coords: [
+			 27.535514831542965,
+                         53.90772909283139
+		],
+		n: 10,
+		address: 'ул. М. Танка, 16',
+		artists: [
+			't_kandratsienka'
+		],
+		landmark: ''
+	},
+        {
+		coords: [
+			27.544972300529476,
+                        53.928188957536584
+		],
+		n: 11,
+		address: 'Старовиленский тракт, 28, к1',
+		artists: [
+			'n_zaloznaya'
+		],
+		landmark: ''
+	},
+	
 ]
